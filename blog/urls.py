f0c00301b641edab138edcd4bdb2c92e4d717518
David Tran <trandavidq@gmail.com>
2021-01-22 20:40:05 -0500

    added login

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import  path
 
+app_name = "blog"
 urlpatterns = [
-    path('', views.index,name="index"),
+    path('', views.login,name="login"),
+    path('signup/',views.signup,name="signup"),
+    path('verify/',views.verify,name="verify"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, World")
+def login(request):
+    return render(request,'blog/login.html')
+
+def verify(request):
+    #change: handles authentication
+    username = request.POST['username']
+    password = request.POST['password']
+    print(username)
+    print(password)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        auth_login(request, user)
+        return HttpResponse("Login successful")
+        
+    else:
+        # Return an 'invalid login' error message.
+        return HttpResponse("Login unsuccessful")
+    
+def signup(request):
+    return render(request,'blog/signup.html')
