7d08bbb126a540f35d7f6fb0955fe79d2d255fbf
David Tran <trandavidq@gmail.com>
2021-01-23 21:20:18 -0500

    Added basic functionalities. Things to fix: Posts created when page is refreshed

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.login,name="login"),
     path('signup/',views.signup,name="signup"),
     path('verify/',views.verify,name="verify"),
-    path('logout/',views.logout,name="logout")
+    path('logout/',views.logout,name="logout"),
+    path('signup/',views.signup,name="signup"),
+    path('register/',views.register,name="register"),
+    path('home/',views.home,name="home"),
+    path('post',views.post,name="post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.urls import reverse
+from .models import blog_post
 # Create your views here.
 def login(request):
     return render(request,'blog/login.html')
@@ -16,6 +17,7 @@ def verify(request):
     user = authenticate(username=username, password=password)
     context= {
         'user':user,
+        'user_posts':user.blog_post_set.all(),
     }
     if user is not None:
         auth_login(request, user)
@@ -28,6 +30,47 @@ def verify(request):
 def signup(request):
     return render(request,'blog/signup.html')
 
+def register(request):
+    #registers the user from the signup. POST requests from signup.html go here
+    username=request.POST['username']
+    password=request.POST['password']
+    user=authenticate(username=username, password=password)
+    if user is not None:
+        #user is already in db, so redirect to login page
+        print('Existing user')
+        context={
+            'uName':user.username,
+            'pWord':user.password,
+        }
+        return render(request,'blog/login.html',context)
+
+        
+    else:
+        #create and register new user, go to homepage
+        new_user=User.objects.create_user(username=username,password=password)
+        context={
+            'user':new_user,
+            'user_posts':new_user.blog_post_set.all(),
+        }
+        auth_login(request, new_user)
+        return render(request,'blog/home.html',context)
+
+    
+
 def logout(request):
     auth_logout(request)
     return redirect('blog:login')
+
+def home(request):
+    return render(request,'blog/home.html')
+
+def post(request):
+    post_title = request.POST['title']
+    post_body = request.POST['body']
+    author= request.user
+    post = blog_post.objects.create(user=author,title=post_title,body=post_body)
+    context={
+        'user':author,
+        'user_posts':author.blog_post_set.all(),
+    }
+    return render(request,'blog/home.html',context)
